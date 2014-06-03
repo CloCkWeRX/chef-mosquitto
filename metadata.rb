948108062009368ba7c539941387bdb2da0190c4
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-06-03 13:48:48 +0930

    Mark OSX as supported (via homebrew)

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.0.1"
 recipe            "mosquitto", "Includes the recipe to configure the broker"
 
-%w{ debian ubuntu }.each do |os|
+%w{ debian ubuntu mac_os_x }.each do |os|
   supports os
 end
 
