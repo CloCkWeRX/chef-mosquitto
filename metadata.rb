e4841205be9bbbb1f86d137bd5a0c69cec27f1c7
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-06-03 13:23:39 +0930

    Add dep on apt cookbook

---
+++ metadata.rb
@@ -0,0 +1,14 @@
+name      "mosquitto"
+maintainer        "Daniel O'Connor"
+maintainer_email  "daniel.oconnor@gmail.com"
+license           "MIT"
+description       "Installs and configures mosquitto, an MQTT broker"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "0.0.1"
+recipe            "mosquitto", "Includes the recipe to configure the broker"
+
+%w{ debian ubuntu }.each do |os|
+  supports os
+end
+
+depends 'apt'
