046d7a77d3b86121c0453e2a294833687edb61ff
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-06-05 18:22:15 +0930

    Typo

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,13 +1,13 @@
 case node['platform_family']
 when "debian", "ubuntu"
-  defaults['mosquitto']['path'] = '/etc/mosquitto/'
+  default['mosquitto']['path'] = '/etc/mosquitto/'
 else
-  defaults['mosquitto']['path'] = '/tmp/mosquitto'
+  default['mosquitto']['path'] = '/tmp/mosquitto'
 end
 
-defaults['mosquitto']['listeners'] = [
+default['mosquitto']['listeners'] = [
   {port: 1883, addr: '127.0.0.1'}, 
   {port: '8883'}
 ]
-defaults['mosquitto']['tls_version'] = 'tlsv1'
+default['mosquitto']['tls_version'] = 'tlsv1'
 
