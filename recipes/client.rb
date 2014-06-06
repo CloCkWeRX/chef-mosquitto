a0d7bd67e35173e452bb9b095d18a2183abd4afc
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-06-06 14:57:45 +0930

    Update ubuntu packages

--- recipes/client.rb
+++ recipes/client.rb
@@ -1,18 +1,12 @@
 client_packages = %w{mosquitto-clients libmosquitto1 libmosquitto-devel libmosquittopp1 libmosquittopp-devel python-mosquitto}
 
 case node['platform']
-when 'ubuntu'
-
-when 'debian'
-
-when "mac_os_x"
-  # Supported by brew
+when 'ubuntu', 'debian'
+  client_packages = %w{mosquitto-clients libmosquitto0 libmosquitto0-dev libmosquittopp0 libmosquittopp0-dev python-mosquitto}
 when "redhat", "centos", "scientific", "fedora", "arch", "amazon"
- 
   client_packages = %w{mosquitto-clients libmosquitto1 libmosquitto-devel libmosquittopp1 libmosquittopp-devel python-mosquitto}
 end
 
-# Todo different recipe?
 client_packages.each do |pkg|
   package pkg
 end
