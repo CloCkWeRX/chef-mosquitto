363dfd41448c3447e583772f1e7da7ce0de270d8
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-06-06 13:05:21 +0930

    Add yum

--- metadata.rb
+++ metadata.rb
@@ -7,8 +7,9 @@ long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.0.1"
 recipe            "mosquitto", "Includes the recipe to configure the broker"
 
-%w{ debian ubuntu mac_os_x }.each do |os|
+%w{ debian ubuntu mac_os_x redhat centos scientific fedora arch amazon }.each do |os|
   supports os
 end
 
 depends 'apt'
+depends 'yum'
