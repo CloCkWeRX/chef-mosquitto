1d77e204fb848433487f9703f78707bc19272281
Robin Eggenkamp <robin@edubits.nl>
2016-06-15 22:26:51 +0200

    Update client packages for Debian Jessie and later

--- recipes/client.rb
+++ recipes/client.rb
@@ -1,10 +1,16 @@
 client_packages = %w{mosquitto-clients libmosquitto1 libmosquitto-devel libmosquittopp1 libmosquittopp-devel python-mosquitto}
 
 case node['platform']
-when 'ubuntu', 'debian'
-  client_packages = %w{mosquitto-clients libmosquitto0 libmosquitto0-dev libmosquittopp0 libmosquittopp0-dev python-mosquitto}
-when "redhat", "centos", "scientific", "fedora", "arch", "amazon"
-  client_packages = %w{mosquitto-clients libmosquitto1 libmosquitto-devel libmosquittopp1 libmosquittopp-devel python-mosquitto}
+  when 'ubuntu'
+    client_packages = %w{mosquitto-clients libmosquitto0 libmosquitto0-dev libmosquittopp0 libmosquittopp0-dev python-mosquitto}
+  when 'debian'
+    if node['platform_version'].to_i <= 7
+      client_packages = %w{mosquitto-clients libmosquitto0 libmosquitto0-dev libmosquittopp0 libmosquittopp0-dev python-mosquitto}
+    else
+      client_packages = %w{mosquitto-clients libmosquitto1 libmosquitto-dev libmosquittopp1 libmosquittopp-dev python-mosquitto}
+    end
+  when "redhat", "centos", "scientific", "fedora", "arch", "amazon"
+    client_packages = %w{mosquitto-clients libmosquitto1 libmosquitto-devel libmosquittopp1 libmosquittopp-devel python-mosquitto}
 end
 
 client_packages.each do |pkg|
